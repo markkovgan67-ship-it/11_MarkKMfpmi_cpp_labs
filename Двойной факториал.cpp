02dd19cd3ae04659531f57693d49610f1db47650
markkovgan67-ship-it <markkovgan67@gmail.com>
2025-09-29 22:26:55 +0300

    Двойной факториал
    Двойной факториал

---
+++ Двойной факториал.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int k;
+    cin >> k;
+
+    int chet = 1;  
+    int nechet = 1; 
+
+    for (int i = 1; i <= k; i++)
+    {
+        if (i % 2 == 0) {
+            chet *= i;
+        }
+        else {
+            nechet *= i;
+        }
+    }
+
+    cout << "mult chet: " << chet << endl;
+    cout << "mult nechet: " << nechet << endl;
+
+    return 0;
+}

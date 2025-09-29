2720305299797654aa059293134dd1e56a9d6d20
markkovgan67-ship-it <markkovgan67@gmail.com>
2025-09-29 22:16:38 +0300

    общие делители
    общие делители

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, c, min,i;
+	cin >> n >> c;
+	min = n;
+	if (c < n) min = c;
+		for (int i = 1; i <= min; i++)
+		{
+			if (n % i == 0 && c % i == 0) 
+			{
+				cout << i << " ";
+			}
+		 
+
+
+		}
+			cout << "Your answer";
+				
+
+	return 0;   
+}
+

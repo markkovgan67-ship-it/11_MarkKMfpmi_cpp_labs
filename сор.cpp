5f88ee58ac8a86b3bde2232fbfa8a3e427fc63a4
markkovgan67-ship-it <markkovgan67@gmail.com>
2025-10-14 10:22:51 +0300

    сортировка пузырьком
    сортировка пузырьком

---
+++ сор.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int n;
+    int result;
+    const int max_lenght = 1000;
+    int arr[max_lenght];
+    std::cout << "Enter number element nassive";
+    std::cin >> n;
+    for ( int i = 0; i < n; i++)
+    {
+    }
+    
+
+
+    
+
+             for (int i = 0; i < n ; i++)
+             {
+                 for (int j = 0; j < n-1-i; j++)
+                 {
+                     if (arr[j] > arr[j + 1]) 
+                     {
+
+                         std::swap(j, j + 1);
+                     }
+                     std::cout << arr[i];
+                 }
+
+             }
+              
+    return 0;
+}
+

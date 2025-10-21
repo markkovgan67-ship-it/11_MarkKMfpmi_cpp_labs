75d3a7cb957856646df3d6f402cae17a267e5fcb
markkovgan67-ship-it <markkovgan67@gmail.com>
2025-10-21 10:25:21 +0300

    сортировка матриц
    сортировка матриц

---
+++ sort matrix.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+void bubble (int* a, int n)
+{
+	int n;
+	for (int i = 0; i < n-1; i++)
+	{
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			if (a[j]>a[j+1])
+			{
+				int m;
+				m = a[j];
+				a[j] = a[j + 1];
+				a[j+1]=m
+			}
+		}
+	}	
+}
+void sortpodshet (int arr[], int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		[arr[i]];
+	}
+	for (int j = 0; j < n; j++)
+	{
+		while arr[i] != 0
+	  {
+		  arr[k] = i;
+		  --count[i];
+	  }
+	}
+}
+void paste(int x,int n)
+{
+	for (int i = 1; i < n; i++)
+	{
+
+	}
+}
+int main()
+{    
+	int i, j, v;
+	std::cout << "Enter number elements of massive";
+	std::cout << "rows:";
+	int n;
+	std::cin >> n;
+	std::cout << "cols:";
+	int k;
+	std::cin >> k; 
+	int matrix[][];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			
+		}
+	}
+	std::cout << "Choose sort matrix:";
+	std::cout << "enter 1 of 3:";
+	std::cin >> v;
+	if(v=1)
+	{
+     bubble ()
+
+
+	}
+	   
+	   
+
+
+
+
+
+
+
+
+
+
+ 
+}
+

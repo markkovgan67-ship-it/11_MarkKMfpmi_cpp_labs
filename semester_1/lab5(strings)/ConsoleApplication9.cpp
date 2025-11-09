2d4736d8f7da3be02c79bbf9a006c6ca65fa0c95
markkovgan67-ship-it <markkovgan67@gmail.com>
2025-11-09 17:04:56 +0300

    Задание 8

---
+++ semester_1/lab5(strings)/ConsoleApplication9.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Enter your string: ";
+    std::string line;
+    std::getline(std::cin, line);
+
+    std::string result = "";
+    std::string word = "";
+    bool foundDigits = false;
+
+    for (char c : line) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                if (isdigit(word[0])) foundDigits = true;
+                if (foundDigits) result = word + " " + result;
+                else result += word + " ";
+                word = "";
+            }
+        }
+        else {
+            word += c;
+        }
+    }
+
+    if (!word.empty()) {
+        if (isdigit(word[0])) foundDigits = true;
+        if (foundDigits) result = word + " " + result;
+        else result += word + " ";
+    }
+
+    std::cout << "Answer: " << result << std::endl;
+    return 0;
+}

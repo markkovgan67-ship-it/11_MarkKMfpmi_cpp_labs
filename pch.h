6ad3ee35bc2c7056a494159a05bd6cd324cf7b28
markkovgan67-ship-it <markkovgan67@gmail.com>
2025-12-10 10:11:01 +0300

    pch.h

--- pch.h
+++ pch.h
@@ -1,14 +1,15 @@
-// pch.h - добавьте этот код
+
 #ifndef PCH_H
 #define PCH_H
 
-// ƒобавл€ем часто используемые заголовки
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
 
-// Google Test заголовок
+
 #include <gtest/gtest.h>
 
-#endif // PCH_H
+
+#endif 
